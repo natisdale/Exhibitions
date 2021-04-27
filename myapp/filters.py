44f41c314f4338a01f47c144ffdbcc1433d72cbc
Nathan Tisdale <natisdale@mail.csuchico.edu>
2021-04-27 17:55:48 +0000

    Updating styles and filters

--- myapp/filters.py
+++ myapp/filters.py
@@ -12,8 +12,8 @@ class ExhibitionFilter(django_filters.FilterSet):
     firstName = CharFilter(field_name='student__first_name', lookup_expr='icontains')
     title = CharFilter(field_name='title', lookup_expr='icontains')
     categories = ModelChoiceFilter(queryset=categories)
-    startDate__gt = NumberFilter(field_name='startDate', lookup_expr='year__gt')
-    startDate__lt = NumberFilter(field_name='startDate', lookup_expr='year__lt')
+    startDate_year__gt = NumberFilter(field_name='startDate', lookup_expr='year__gt')
+    startDate_year__lt = NumberFilter(field_name='startDate', lookup_expr='year__lt')
 
     class Meta:
         model = Exhibition
