f9dbd8e97aee438c47e5d48168136e42ca5a9e3b
Nathan Tisdale <natisdale@mail.csuchico.edu>
2021-04-28 04:16:04 +0000

    Adding Azure Storage configuration

--- myproject/azure.py
+++ myproject/azure.py
@@ -1,4 +1,5 @@
 import os
+from storages.backends.azure_storage import AzureStorage
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
@@ -12,8 +13,29 @@ DATABASES = {
         'PASSWORD': os.environ['DBPASS'] 
     }
 }
-
-
 SECRET_KEY = os.getenv('SECRET_KEY')
-
 DEBUG = os.environ.get('DEBUG')
+
+AZURE_ACCOUNT_NAME = os.environ['AZURE_ACCOUNT_NAME']
+AZURE_STORAGE_KEY = os.environ['AZURE_STORAGE_KEY']
+AZURE_MEDIA_CONTAINER = os.environ['AZURE_MEDIA_CONTAINER']
+AZURE_STATIC_CONTAINER = os.environ['AZURE_STATIC_CONTAINER']
+
+class AzureStaticStorage(AzureStorage):
+    account_name = AZURE_ACCOUNT_NAME
+    account_key = AZURE_STORAGE_KEY
+    azure_container = 'static'
+    expiration_secs = None
+
+class AzureMediaStorage(AzureStorage):
+    account_name = AZURE_ACCOUNT_NAME
+    account_key = AZURE_STORAGE_KEY
+    azure_container = 'media'
+    expiration_secs = None
+
+
+DEFAULT_FILE_STORAGE = 'AzureMediaStorage'
+STATICFILES_STORAGE = 'AzureStaticStorage'
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_STATIC_CONTAINER}'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_MEDIA_CONTAINER}'
